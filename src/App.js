1b7c4f007246c773534b5c2542d3b42e231cb498
zak <codedpills@gmail.com>
2020-02-29 14:35:08 +0000

    feat: add component to add and display users

--- src/App.js
+++ src/App.js
@@ -1,26 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [
+        {
+          name: "Ahmed Zaky",
+          email: "ahmed@email.com",
+          gen: 1
+        },
+        {
+          name: "Kwesi Arthur",
+          email: "kwesi@email.com",
+          gen: 6
+        },
+        {
+          name: "Kuame Uegine",
+          email: "kuame@email.com",
+          gen: 3
+        },
+        {
+          name: "King Promise",
+          email: "king@email.com",
+          gen: 3
+        }
+      ],
+      name: "",
+      email: "",
+      gen: ""
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+    console.log(this.state.name);
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      gen: this.state.gen
+    };
+    this.setState({
+      users: [...this.state.users, newUser],
+      name: "",
+      email: "",
+      gen: ""
+    });
+  };
+
+  render() {
+    const users = this.state.users.map((person, index) => {
+      return (
+        <div key={index}>
+          <h4>{person.name}</h4>
+          <p>{person.email}</p>
+          <p>{person.gen}</p>
+        </div>
+      );
+    });
+
+    return (
+      <div className="App">
+        <div className="form-container">
+          <h1>ADD NEW USER</h1>
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-control">
+              <label>Full name</label>
+              <input
+                type="text"
+                name="name"
+                value={this.state.name}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="form-control">
+              <label>Email</label>
+              <input
+                type="email"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div className="form-control">
+              <label>Gen</label>
+              <input
+                type="number"
+                name="gen"
+                value={this.state.gen}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div>
+              <button type="submit">Save user</button>
+            </div>
+          </form>
+        </div>
+        <div className="users-container">
+          <h1>USERS:</h1>
+          {users}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
