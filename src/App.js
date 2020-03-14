f08923890b324112fc230223b218bc13eef188b9
zak <codedpills@gmail.com>
2020-03-14 18:20:35 +0000

    chore: display User and AddUserForm components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import AddUserForm from "./components/AddUserForm";
+import User from "./components/User";
 import "./App.css";
 
 class App extends Component {
@@ -26,43 +28,25 @@ class App extends Component {
           email: "king@email.com",
           gen: 3
         }
-      ],
-      name: "",
-      email: "",
-      gen: ""
+      ]
     };
   }
 
-  handleChange = e => {
+  addUser = newUser => {
     this.setState({
-      [e.target.name]: e.target.value
-    });
-    console.log(this.state.name);
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    const newUser = {
-      name: this.state.name,
-      email: this.state.email,
-      gen: this.state.gen
-    };
-    this.setState({
-      users: [...this.state.users, newUser],
-      name: "",
-      email: "",
-      gen: ""
+      users: [...this.state.users, newUser]
     });
   };
 
   render() {
     const users = this.state.users.map((person, index) => {
       return (
-        <div key={index}>
-          <h4>{person.name}</h4>
-          <p>{person.email}</p>
-          <p>{person.gen}</p>
-        </div>
+        <User
+          key={index}
+          name={person.name}
+          email={person.email}
+          gen={person.gen}
+        />
       );
     });
 
@@ -70,38 +54,7 @@ class App extends Component {
       <div className="App">
         <div className="form-container">
           <h1>ADD NEW USER</h1>
-          <form onSubmit={this.handleSubmit}>
-            <div className="form-control">
-              <label>Full name</label>
-              <input
-                type="text"
-                name="name"
-                value={this.state.name}
-                onChange={this.handleChange}
-              />
-            </div>
-            <div className="form-control">
-              <label>Email</label>
-              <input
-                type="email"
-                name="email"
-                value={this.state.email}
-                onChange={this.handleChange}
-              />
-            </div>
-            <div className="form-control">
-              <label>Gen</label>
-              <input
-                type="number"
-                name="gen"
-                value={this.state.gen}
-                onChange={this.handleChange}
-              />
-            </div>
-            <div>
-              <button type="submit">Save user</button>
-            </div>
-          </form>
+          <AddUserForm addNewUser={this.addUser} />
         </div>
         <div className="users-container">
           <h1>USERS:</h1>
