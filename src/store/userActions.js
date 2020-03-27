fed4e63c5410d653c3c793232c95250b048d94e7
zak <codedpills@gmail.com>
2020-03-27 02:34:06 +0000

    chore: add action generators

--- src/store/userActions.js
+++ src/store/userActions.js
@@ -1,5 +1,5 @@
 // action creator - function that just returns an action
-const addUser = (user) => {
+export const addUser = (user) => {
     return {
         type: 'ADD_USER',
         payload: user
